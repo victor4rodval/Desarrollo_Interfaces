0a7c944a1bdf2f71eda4aa8e63ff212313bb0dc8
victor4rodval <>
2022-09-29 17:19:20 +0200

    Añadido ejercicio botones y desplegables

---
+++ src/main/java/Botones/PracticaBotones.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Botones;
+
+/**
+ *
+ * @author 6002285
+ */
+public class PracticaBotones {
+    public static void main(String[] args) {
+        
+        Botones b = new Botones();
+        b.setVisible(true);
+    }
+    
+}
---
+++ src/main/java/Primeros_ejercicios/Practica2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Primeros_ejercicios;
+
+/**
+ *
+ * @author 6002285
+ */
+public class Practica2 {
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+        
+    }
+}
